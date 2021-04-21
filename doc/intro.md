fd233ce840a929e43285db989f3e6bd30505ea3a
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-04-21 14:17:28 +0100

    git init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sotify
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sotify/core_test.clj
@@ -0,0 +1,7 @@
+(ns sotify.core-test
+  (:require [clojure.test :refer :all]
+            [sotify.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
